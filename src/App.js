8af8acf9cad641a1dfa91b3c45bb956b428633e3
marcesalas96 <marcesalas1996@gmail.com>
2022-05-17 00:33:03 -0300

    Probando React App

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h5>Marcelo Salas</h5>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proabando React
         </p>
         <a
           className="App-link"
